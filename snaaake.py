cf36258bd673f7efd7c4f066b311c8a2f3507bb9
Parvesh Yadav <parveshyadav@Parveshs-Air.lan>
2024-03-19 18:12:06 -0700

    looping around screen

--- snaaake.py
+++ snaaake.py
@@ -2,31 +2,54 @@ import pygame
 
 pygame.init()
 
-SCREEN_WIDTH = 800
+SCREEN_WIDTH = 600
 SCEEEN_HEIGHT = 600
+SNAKE_X_START=300
+SNAKE_Y_START=250
+SNAKE_HEIGHT=40
+SNAKE_LENGTH=40
+GREEN=(50,205,50)
+SNAKE_COLOR=(255,255,255,0)
+SPEED=1
 
 screen=pygame.display.set_mode((SCREEN_WIDTH,SCEEEN_HEIGHT))
-snake = pygame.Rect((300,250,40,40))
+
+pygame.display.set_caption("Snaaake dash")
+
+snake = pygame.Rect((SNAKE_X_START,SNAKE_Y_START,SNAKE_HEIGHT,SNAKE_LENGTH))
+
 
 run=True
 
 while run:
-       screen.fill((50,205,50))
-       pygame.draw.rect(screen,(255,255,255),snake)
-
-       key=pygame.key.get_pressed()
-       if key[pygame.K_UP]==True:
-          snake.move_ip(0,-1) 
-       if key[pygame.K_DOWN]==True:
-          snake.move_ip(0,1)
-       if key[pygame.K_LEFT]==True:
-          snake.move_ip(-1,0)
-       if key[pygame.K_RIGHT]==True:
-          snake.move_ip(1,0)      
+       screen.fill(GREEN)
+       pygame.draw.rect(screen,SNAKE_COLOR,snake)
 
        for event in pygame.event.get():
            if event.type==pygame.QUIT:
               run = False
-       pygame.display.update(
-)
+     
+       if snake.x>=600:
+         snake.update(0,snake.y,SNAKE_HEIGHT,SNAKE_LENGTH)
+       elif snake.x<=0:
+         snake.update(600,snake.y,SNAKE_HEIGHT,SNAKE_LENGTH)
+
+       if snake.y>=600:
+         snake.update(snake.x,0,SNAKE_HEIGHT,SNAKE_LENGTH)
+       elif snake.y<=0:
+         snake.update(snake.x,600,SNAKE_HEIGHT,SNAKE_LENGTH)
+
+       key=pygame.key.get_pressed()
+       if key[pygame.K_UP]==True:
+          snake.move_ip(0,-SPEED) 
+       if key[pygame.K_DOWN]==True:
+          snake.move_ip(0,SPEED)
+       if key[pygame.K_LEFT]==True:
+          snake.move_ip(-SPEED,0)
+       if key[pygame.K_RIGHT]==True:
+          snake.move_ip(SPEED,0) 
+
+      
+     
+       pygame.display.update()
 pygame.quit()
