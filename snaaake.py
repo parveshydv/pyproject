20511e2abdbb605ad8f1567a77dc509137787394
Parvesh Yadav <parveshahir@gmail.com>
2024-06-19 22:36:54 -0700

    testing

--- snaaake.py
+++ snaaake.py
@@ -9,16 +9,19 @@ SCREEN_WIDTH = 800
 SCEEEN_HEIGHT = 800
 SNAKE_X_START=300
 SNAKE_Y_START=250
+FOOD_X_START = 200
+FOOD_Y_START = 200
 SNAKE_HEIGHT=40
 SNAKE_LENGTH=40
-SCREEN_COLOR="black"
-SNAKE_COLOR="white"
+SCREEN_COLOR="white"
+SNAKE_COLOR="green"
+FOOD_LENGTH=40
 SPEED=3
 
 screen=pygame.display.set_mode((SCREEN_WIDTH,SCEEEN_HEIGHT))
 pygame.display.set_caption("Snaaake dash")
 snake = pygame.Rect((SNAKE_X_START,SNAKE_Y_START,SNAKE_HEIGHT,SNAKE_LENGTH))
-food = pygame.Rect((SNAKE_X_START,SNAKE_Y_START,SNAKE_HEIGHT,SNAKE_LENGTH))
+food = pygame.Rect((FOOD_X_START,FOOD_X_START,SNAKE_HEIGHT,SNAKE_LENGTH))
 
 
 def delay(int):
@@ -46,6 +49,10 @@ def motion():
     if key[pygame.K_RIGHT]==True:
        snake.move_ip(1,0)
 
+def size():
+   if snake.x == food.x and snake.y == food.y:
+      SNAKE_LENGTH = SNAKE_LENGTH + FOOD_LENGTH
+      
 
 run=True
 
@@ -59,7 +66,9 @@ while run:
               run = False
       
        delay(SPEED)
-       motion()       
+       motion()
+       size()
+              
        pygame.display.update()
 pygame.quit()
 
